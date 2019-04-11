f147d1cca7811ec5070d047670c570ea340d4573
Matt Clarke <mclarke0805@gmail.com>
2019-04-11 20:25:39 +0100

    Forward touches on foreground widgets to background behind icons

--- Tweak/Supporting/XENHTouchForwardingRecognizer.m
+++ Tweak/Supporting/XENHTouchForwardingRecognizer.m
@@ -151,8 +151,11 @@
     
     // Handle safe area - views
     // TODO: Whitelist the foreground SBHTML here, as a view that forwarding can occur on
-    if (![[arg3 class] isEqual:objc_getClass("SBRootIconListView")] &&
-        ![[arg3 class] isEqual:objc_getClass("IWWidgetsView")]) {
+    if (![[arg3 class] isEqual:objc_getClass("SBRootIconListView")] && // Icons
+        ![[arg3 class] isEqual:objc_getClass("IWWidgetsView")] && // iWidgets
+        ![[arg3.layer name] isEqualToString:@"RootContent"] && // WKWebView
+        ![[arg3 class] isEqual:objc_getClass("UIWebBrowserView")]) // UIWebView
+    {
         
         XENlog(@"Not forwarding touch; touchView is not SBRootIconListView (is %@)", [arg3 class]);
         return NO;
