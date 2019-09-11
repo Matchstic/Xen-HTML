2512205181129bdb82a1b3aaf2008af3e0237cb8
Matt Clarke <mclarke0805@gmail.com>
2019-09-11 21:54:07 +0100

    Move objc_msgSend out of the global namesapce

--- deps/libwidgetdata/lib/Middleware/src/infostats2/index.ts
+++ deps/libwidgetdata/lib/Middleware/src/infostats2/index.ts
@@ -3,10 +3,11 @@ import NativeInterface from '../native-interface';
 
 export default class IS2Middleware implements XenHTMLMiddleware {
     public initialise(parent: NativeInterface): void {
-
+        // Register observers to the data providers
     }
-}
 
-export function objc_msgSend(object: any, selector: string): void {
-
+    public objc_msgSend(object: any, selector: string): void {
+        // stringify the incoming object, and figure out what to call
+        // Note: native-side parser needs to string-replace 'objc_msgSend' with '_xenhtml_middleware.infostats2.objc_msgSend'
+    }
 }
