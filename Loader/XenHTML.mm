a8d22b3300522a41a852d1ca589262806ae9e1e9
Matt Clarke <mclarke0805@gmail.com>
2020-12-23 15:29:33 +0000

    Revert "Move WidgetInfo to be loaded via Loader"
    This reverts commit 219f4583b06e3546c2d9afb77a93640148368fb8.

--- Loader/XenHTML.mm
+++ Loader/XenHTML.mm
@@ -81,7 +81,7 @@ static void _logos_method$_ungrouped$SpringBoard$applicationDidFinishLaunching$(
     
     BOOL hasBatteryManager = [[NSFileManager defaultManager] fileExistsAtPath:@"/Library/MobileSubstrate/DynamicLibraries/XenHTML/XenHTML_ZBatteryManager.dylib"];
     BOOL hasWebGL = [[NSFileManager defaultManager] fileExistsAtPath:@"/Library/MobileSubstrate/DynamicLibraries/XenHTML_WebGL.dylib"];
-    BOOL hasWidgetInfo = [[NSFileManager defaultManager] fileExistsAtPath:@"/Library/MobileSubstrate/DynamicLibraries/XenHTML/XenHTML_WidgetInfo.dylib"];
+    BOOL hasWidgetInfo = [[NSFileManager defaultManager] fileExistsAtPath:@"/Library/MobileSubstrate/DynamicLibraries/XenHTML_WidgetInfo.dylib"];
     
     
     if (!isRootless) {
@@ -114,12 +114,6 @@ static __attribute__((constructor)) void _logosLocalCtor_bdbb22fb(int __unused a
     }
     
     
-    if ([[NSFileManager defaultManager] fileExistsAtPath:@"/Library/MobileSubstrate/DynamicLibraries/XenInfo.dylib"]) {
-        dlopen("/Library/MobileSubstrate/DynamicLibraries/XenInfo.dylib", RTLD_NOW);
-    }
-    
-    
-    dlopen("/Library/MobileSubstrate/DynamicLibraries/XenHTML/XenHTML_WidgetInfo.dylib", RTLD_NOW);
     dlopen("/Library/MobileSubstrate/DynamicLibraries/XenHTML/XenHTML_ZBatteryManager.dylib", RTLD_NOW);
 }
 
